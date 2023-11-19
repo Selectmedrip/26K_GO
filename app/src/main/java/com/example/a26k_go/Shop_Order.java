0d1b834f8676bb08136056fd937a4613dd7ba6f2
malado1's <arman.papiants@yandex.ru>
2023-11-19 16:05:34 +0300

    Update FULL Function

---
+++ app/src/main/java/com/example/a26k_go/Shop_Order.java
@@ -0,0 +1,14 @@
+package com.example.a26k_go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Shop_Order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_order);
+    }
+}
